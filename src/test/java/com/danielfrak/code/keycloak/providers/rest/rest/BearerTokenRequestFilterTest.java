3f03b78814f4e25467b31cd11d4df8ecd2bc6e20
Todd Kazakov <todd.kazakov@gmail.com>
2020-08-10 16:27:47 +0300

    Add tests for bearer token request filter

---
+++ src/test/java/com/danielfrak/code/keycloak/providers/rest/rest/BearerTokenRequestFilterTest.java
@@ -0,0 +1,35 @@
+package com.danielfrak.code.keycloak.providers.rest.rest;
+
+import org.jboss.resteasy.specimpl.MultivaluedMapImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.core.MultivaluedMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BearerTokenRequestFilterTest {
+    @Mock
+    private ClientRequestContext context;
+
+    @Test
+    public void filter() {
+        String token = "secret-api-token";
+        String expectedAuthorizationHeader = "Bearer " + token;
+
+        BearerTokenRequestFilter requestFilter = new BearerTokenRequestFilter(token);
+        MultivaluedMap<String, Object> headers = new MultivaluedMapImpl<>();
+        when(context.getHeaders()).thenReturn(headers);
+
+        requestFilter.filter(context);
+
+        Object result = headers.getFirst("Authorization");
+        assertNotNull(result);
+        assertEquals(result.toString(), expectedAuthorizationHeader);
+    }
+}
