117b14cb10869bf5569bdbcc685217f42754fe86
Georg Moser <georg-moser@gmx.at>
2023-02-27 10:09:08 +0100

    Update cypress

---
+++ docker/e2e/cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 20000,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  mailHogUrl: 'http://localhost:8025',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8024',
+  },
+})
